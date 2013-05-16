b53af3daf1342745c707fdf2d38da57a57a6c8c4
mavis <545089111@qq.com>
2013-05-16 18:10:52 +0800

    基本雏形搭建完成

---
+++ data.js
@@ -0,0 +1,29 @@
+var DEPARTMENT_MOBILE = 1;
+var DEPARTMENT_DESIGN = 2;
+var DEPARTMENT_TEST1 = 3;
+var DEPARTMENT_TEST2 = 4;
+
+var MOBILE_USERS =
+    [
+        {"uname": "李歆扬", "unum": "90500"},
+        {"uname": "廖青", "unum": "83124"},
+        {"uname": "胡巍巍", "unum": "89232"},
+        {"uname": "徐山", "unum": "90593"}
+    ];
+
+
+var DESIGN_USERS =
+    [
+        {"uname": "赵阳", "unum": "82664"},
+        {"uname": "许海芝", "unum": "89176"},
+        {"uname": "李诗娇", "unum": "89866"},
+        {"uname": "王鹏", "unum": "91579"}
+    ];
+
+var DEPARTMENT =
+    [
+        {"dpname": "移动产品部", "dpnum" : 1},
+        {"dpname": "交互设计部", "dpnum" : 2},
+        {"dpname": "test1部", "dpnum" : 3},
+        {"dpname": "test2部", "dpnum" : 4}
+    ];
