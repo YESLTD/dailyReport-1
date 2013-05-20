0bf6fce7c417b13099de53f6573f906d5949dcc2
mavis <545089111@qq.com>
2013-05-20 14:04:40 +0800

    页面交互体验的一些修改

--- test.js
+++ test.js
@@ -6,110 +6,145 @@ var http = require('http'),
 //    mongoose = require('mongoose'),
     mongodb = require('mongodb');
 
-var server = new mongodb.Server("192.168.0.133", 27017); //连接Mongodb
+//var server = new mongodb.Server("192.168.0.133", 27017); //连接Mongodb
+var server = new mongodb.Server("localhost", 27017); //连接Mongodb
 var conn = new mongodb.Db("dailyReport", server, {safe: false});
 
-fs.readFile("index.html", function(err, data) {
 
-    http.createServer(function (req, res) {
-        var pathname = url.parse(req.url).pathname;
-        var query = url.parse(req.url).query;
-        console.log(" --------------- pathname : ", pathname, "\n ------------ query : ", query);
+http.createServer(function (req, res) {
+    var pathname = url.parse(req.url).pathname;
+    var query = url.parse(req.url).query;
+    console.log(" --------------- pathname : ", pathname, "\n ------------ query : ", query);
 
-
-//        res.setHeader("Content-Type", "application/x-javascript");
-//        res.write(data);
-//        res.end();
-
-        // 加载用户
-        if (pathname == "/loadUsers") {
-            var udepnum = querystring.parse(query).udepnum;
-            console.log("------------ 部门号 ------------ " + udepnum);
-            conn.open(function(err, db) {
-                db.collection("departments", function(err, collection){
-                    collection.find(function(err, cursor) {
-                        cursor.toArray(function(err, data) {
-                            for (var i=0; i<data.length; i++) {
-                                if (data[i].dpnum == udepnum) {
-                                    console.log("\n -------------- 加载用户 ------------- \n", data[i]["dusers"]);
-                                    res.writeHead(200, {"Content-Type": "application/json"});
-                                    res.end(JSON.stringify({"dusers": data[i]["dusers"]}));
-                                    break;
-                                }
-                            }
-                            conn.close();
-                        });
-                    });
-                });
-            });
-            // 加载部门
-        } else if (pathname == "/loadDepartments") {
-            conn.open(function(err, db) {
-                db.collection("departments", function(err, collection){
-                    collection.find().toArray(function(err, data) {
-                        console.log("\n -------------- 加载部门 ------------- \n", typeof data, data);
-                        res.writeHead(200, {"Content-Type": "application/json"});
-                        res.end(JSON.stringify({"departments": data}));
-                        conn.close();
-                    });
-                });
-            });
-            // 加载指定部门指定日期的用户
-        } else if (pathname == "/loadDetail") {
-            var udep = querystring.parse(query).udep;
-            var querydate = querystring.parse(query).querydate;
-            console.log("------------ udep ------------ " + udep);
-            console.log("------------ querydate ------------ " + querydate);
-            conn.open(function(err, db) {
-                db.collection("dailyLogs", function(err, collection) {
-                    collection.find({logger_dpid: udep, date: querydate}).toArray(function(err, data) {
-                        console.log("------------- data ------------ \n", data);
-                        var results = new Array();
+    // 加载用户
+    if (pathname == "/loadUsers") {
+        var udepnum = querystring.parse(query).udepnum;
+        console.log("------------ 部门号 ------------ " + udepnum);
+        conn.open(function(err, db) {
+            db.collection("departments", function(err, collection){
+                collection.find(function(err, cursor) {
+                    cursor.toArray(function(err, data) {
                         for (var i=0; i<data.length; i++) {
-                            console.log("-------- data[i]--------- \n", data[i], data[i].modifylog.length);
-                            var log = {
-                                "logger_name": data[i].logger_name,
-                                "done": data[i].modifylog[data[i].modifylog.length - 1].done,
-                                "plan": data[i].modifylog[data[i].modifylog.length - 1].plan,
-                                "remark": data[i].modifylog[data[i].modifylog.length - 1].remark
+                            if (data[i].dpnum == udepnum) {
+                                console.log("\n -------------- 加载用户 ------------- \n", data[i]["dusers"]);
+                                res.writeHead(200, {"Content-Type": "application/json"});
+                                res.end(JSON.stringify({"dusers": data[i]["dusers"]}));
+                                break;
                             }
-                            results.push(log);
                         }
-                        console.log("--------- 加载指定部门的日志 --------- \n", typeof results, results);
-                        res.writeHead(200, {"Content-Type": "application/json"});
-                        res.end(JSON.stringify({"dusers": results}));
                         conn.close();
                     });
                 });
             });
-            // 保存/修改日志
-        } else if (pathname == "/submitLog") {
-            var info = "";
-            req.addListener('data', function(data){
-                info += data;
-            }).addListener('end', function(){
-                    info = querystring.parse(info);
-                    saveLog(info, req);
-                });
-        } else if (pathname == "/loadInitInfo") {
-            conn.open(function(err, db) {
-                db.collection("departments", function(err, collection){
-                    collection.find().toArray(function(err, data) {
-                        console.log(" -------------- 部门所有信息 ------------- \n", typeof data, data);
-                        res.writeHead(200, {"Content-Type": "application/json"});
-                        res.end(JSON.stringify({"departments": data}));
-                        conn.close();
-                    });
-                });
-            });
-        }
-        conn.on("close", function() {
-            console.log(" ------------ db close ------------ \n");
         });
+        // 加载部门
+    } else if (pathname == "/loadDepartments") {
+        conn.open(function(err, db) {
+            db.collection("departments", function(err, collection){
+                collection.find().toArray(function(err, data) {
+                    console.log("\n -------------- 加载部门 ------------- \n", typeof data, data);
+                    res.writeHead(200, {"Content-Type": "application/json"});
+                    res.end(JSON.stringify({"departments": data}));
+                    conn.close();
+                });
+            });
+        });
+        // 加载指定部门指定日期的用户
+    } else if (pathname == "/loadDetail") {
+        var udep = querystring.parse(query).udep;
+        var querydate = querystring.parse(query).querydate;
+        console.log("------------ udep ------------ " + udep);
+        console.log("------------ querydate ------------ " + querydate);
+        conn.open(function(err, db) {
+            db.collection("dailyLogs", function(err, collection) {
+                collection.find({logger_dpid: udep, date: querydate}).toArray(function(err, data) {
+                    console.log("------------- data ------------ \n", data);
+                    var results = new Array();
+                    for (var i=0; i<data.length; i++) {
+                        console.log("-------- data[i]--------- \n", data[i], data[i].modifylog.length);
+                        var log = {
+                            "logger_name": data[i].logger_name,
+                            "done": data[i].modifylog[data[i].modifylog.length - 1].done,
+                            "plan": data[i].modifylog[data[i].modifylog.length - 1].plan,
+                            "remark": data[i].modifylog[data[i].modifylog.length - 1].remark
+                        }
+                        results.push(log);
+                    }
+                    console.log("--------- 加载指定部门的日志 --------- \n", typeof results, results);
+                    res.writeHead(200, {"Content-Type": "application/json"});
+                    res.end(JSON.stringify({"dusers": results}));
+                    conn.close();
+                });
+            });
+        });
+        // 保存/修改日志
+    } else if (pathname == "/submitLog") {
+        var info = "";
+        req.addListener('data', function(data){
+            info += data;
+        }).addListener('end', function(){
+            info = querystring.parse(info);
+            saveLog(info, req);
+        });
+        // 加载初始信息
+    } else if (pathname == "/loadInitInfo") {
+        conn.open(function(err, db) {
+            db.collection("departments", function(err, collection){
+                collection.find().toArray(function(err, data) {
+                    console.log(" -------------- 部门所有信息 ------------- \n", typeof data, data);
+                    res.writeHead(200, {"Content-Type": "application/json"});
+                    res.end(JSON.stringify({"departments": data}));
+                    conn.close();
+                });
+            });
+        });
+    // 加载某人当天日志
+    } else if(pathname =="/loadTodayReport") {
+        var info = "";
+        req.addListener('data', function(data){
+            info += data;
+        }).addListener('end', function(){
+            info = querystring.parse(info);
+            queryLog(info, res);
+        });
+    }
 
-    }).listen(8124, "127.0.0.1");
-});
+    conn.on("close", function() {
+        console.log(" ------------ db close ------------ \n");
+    });
 
+}).listen(8124, "127.0.0.1");
+
+function queryLog(info, res) {
+    console.log("-------- 查询当天是否写了日报，POST参数----------- \n", info);
+    conn.open(function(err, db) {
+        db.collection("dailyLogs", function(err, collection) {
+            collection.find({logger_id:info.logger_id, date:info.date}).toArray(function(err, data) {
+                console.log("----------------- 查询结果为 ----------------- \n", data, data.length);
+                if (!data || data=="") {
+                    var results = {
+                        "exist": false
+                    }
+                    console.log("----------- 此人当天未曾写日报 ------------- \n");
+                } else {
+                    console.log("----------- 此人当天已经写过日报 -------------- \n", data, typeof data);
+                    console.log("----------- 此人当天日报记录 --------------\n", data[0].modifylog);
+                    var newestLog = data[0].modifylog[data[0].modifylog.length - 1];
+                    var results = {
+                        "exist": true,
+                        "done": newestLog.done,
+                        "plan": newestLog.plan,
+                        "remark": newestLog.remark
+                    }
+                    console.log("----- results: ---\n", results);
+                }
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({"results": results}));
+                conn.close();
+            });
+        });
+    });
+}
 
 
 // 插入日志
